c81109a0ffdb64b46caf7dd749df498100e166f1
David Liu <kunweiliu@gmail.com>
2019-08-20 15:43:18 -0400

    added code to handle duplicated filename and folder name

--- app.R
+++ app.R
@@ -32,11 +32,12 @@ ui <- fluidPage(
              h2 ("Folder Exploder"),
              p("This application allows you to unravel (explode) a folder and copied the unraveled folder or files to the destination folder"),
              p("If resursive option is selected, all individual files in each subfolder will be copied"),
+             p("If there is duplicate, a number will be appended to end of the file"),
              p("This app is also useful if you want to extra a particular file type from a bunch of folder. 
                  For example, if you enable the resursive option and enter .pdf in the advanced option below, then all the pdf files will be copied"),
              textAreaInput(inputId = "source_folder", label = "Please enter the source directory. (Example: C:\\Users\\john.doe\\Desktop\\folder1)",
                            value = "D:\\Gatech OneDrive\\OneDrive - Georgia Institute of Technology\\R Project\\FolderExploderRshiny\\For Testing\\Test Folder Source"),
-             textAreaInput(inputId = "destination_folder", label = "Please enter the desination directory. (Example:  C:\\Users\\john.doe\\Desktop\\folder2)",
+             textAreaInput(inputId = "destination_folder", label = "Please enter the desination directory. You must create this folder by yourself (Example:  C:\\Users\\john.doe\\Desktop\\folder2)",
                            value = "D:\\Gatech OneDrive\\OneDrive - Georgia Institute of Technology\\R Project\\FolderExploderRshiny\\For Testing\\Test Folder Destination"), 
              wellPanel(
                numericInput(inputId = "depth", label = "Please enter the depth of folder explotion in integer (0 or above).
@@ -146,7 +147,7 @@ server <- function(input, output,session) {
         showModal(modalDialog("The depth you entered is a number but not a valid integer. Please enter a valid integer for depth", easyClose = T))
         stop("The depth you entered is a number but not a valid integer")
       } else if (int_indicator == TRUE & input$depth %%1 != 0){
-        print ("xxx")
+        
         showModal(modalDialog("The depth you entered is a number but not a valid integer. Please enter a valid integer for depth", easyClose = T))
         stop("The depth you entered is a number but not a valid integer")
         
@@ -208,12 +209,11 @@ server <- function(input, output,session) {
         return (T)
       }
       # since only the name after the last slash contains the file name
-      # thus we need to subset the last element after split
+      # thus we need to obtain the basename using the basename function()
       
-      str <- last(str_split(str, "/")[[1]])
+      str <- basename(str) 
       
       if (any(!missing(start_pattern), !missing (contain_pattern), !missing(end_pattern))){
-        print ("condition1")
         if (start_pattern != "") {
           start_pattern <- paste0("^", start_pattern)
         }
@@ -246,24 +246,102 @@ server <- function(input, output,session) {
       bool <- filepath %>% tolower %>% map_lgl(test_reg, start_pattern = input$start_pattern,
                                                contain_pattern = input$contain_pattern,
                                                end_pattern = input$end_pattern)
-      print (bool)
+      #print (bool)
       filepath <- filepath[bool]
       
     }
     
-    print (filepath)
+    #print (filepath)
     print ("finished dealing with regular expression")
     
     
+    # use dataframe and dplyr to add rep number for duplicate
+    DF <-  data.frame(source_filepath = filepath) %>% mutate (source_filepath = file.path(source_filepath)) %>%
+      mutate (source_basename = basename(source_filepath)) %>% 
+      tidyr::separate(source_basename, into = c("source_one", "source_two"),sep = "\\.", remove = F, extra = "drop") %>%
+      mutate (number = 1) %>% group_by(source_basename) %>% mutate (rep = cumsum(number)) %>% #count duplicate
+      mutate (rep = rep - 1) %>% select (-number) %>% 
+      mutate (rep = ifelse(rep == 0, "", paste0(" (",rep,")")))
+    
+    DF_with_dot <- DF %>% filter (!is.na(source_two)) %>% mutate (is_folder = ifelse(dir.exists(source_filepath), 1, 0)) 
+    # append rep to filename
+    DF_with_dot <- DF_with_dot %>% mutate (destination_basename = paste0(source_one, rep,".",source_two))
+    
+    # append rep to filename
+    DF_no_dot <- DF %>% filter (is.na(source_two)) %>% mutate (is_folder = ifelse(dir.exists(source_filepath), 1, 0)) 
+    DF_no_dot <-  DF_no_dot %>% mutate (destination_basename = paste0(source_one, rep))
+    
+    #combine the no_dot and with_dot dataframe together
+    DF_final <- DF_with_dot %>% bind_rows(DF_no_dot) %>% select(-c(source_one, source_two,rep))  %>%
+      mutate (destination_filepath = file.path(input$destination_folder, destination_basename))
+    
+    #pull the sourcefilepath and destination filepath to vector for files
+    source_filepath_vec <- DF_final %>% filter (is_folder == 0)  %>% pull(source_filepath)
+    destination_filepath_vec <- DF_final %>% filter (is_folder == 0) %>% pull(destination_filepath)
+    
+    #pull the sourcefilepath for folders
+    source_filepath_folder_vec <- DF_final %>% filter (is_folder == 1) %>% pull(source_filepath)
+    destination_filepath_folder_vec <- DF_final %>% filter (is_folder == 1) %>% pull(destination_filepath)
+    
+    
+    
     # copy files
+    file.copy(from = source_filepath_vec, to = destination_filepath_vec,recursive = F)
+    print ("finished copying files")
     
-    file.copy(from = filepath , to = input$destination_folder,recursive = T)
     
-    showModal(modalDialog(paste0("The files fitting your criteria of depth or string matching are copied to ( ",
-                                 input$destination_folder, " ).  ", length(filepath), " files are copied.")))
+    # define a function to copy folder
+    
+    copy_folders <- function(source_filepath, destination_folder){
+      # this function allows copying folders from a source_path to a destination folder and add a number if there is duplicate
+      # it take a vector a filepath of folder, and a filepath to a destination folder
+      
+      # create a temp folder with random name
+      temp_folder_name <- paste0("__", sample(1:99999,1), "temp__")
+      temp_folder_filepath = file.path(destination_folder,  temp_folder_name)
+      dir.create(temp_folder_filepath)
+      
+      for (i in seq_along(source_filepath)){
+        
+        stuff_in_destination <- list.files(path = destination_folder,full.names = T, recursive = F, ignore.case = F)
+        # find the folders already in the destination
+        folder_in_destination <- basename(stuff_in_destination[dir.exists(stuff_in_destination)])
+        # if the folder is already in the destination folder, copy to temp folder then rename
+        base_name <- basename(source_filepath[i])
+        rep = sum(folder_in_destination == base_name)
+        print (rep)
+        
+        if (rep == 0) {
+          file.copy(from = source_filepath[i], to = destination_folder,recursive = T)
+        }  else if (rep >= 1) {
+          file.copy(from = source_filepath[i], to = temp_folder_filepath,recursive = T)
+          file.rename(from = file.path(temp_folder_filepath,base_name),
+                      to = file.path(destination_folder, paste0(base_name, " (", rep, ")")))
+        }
+      }
+      
+      # delete the temp folder after done. using unlink function
+      unlink (temp_folder_filepath, recursive = T, force = T)
+    }
+    
+    # start copying folder
+    if (length(source_filepath_folder_vec) > 0){
+      print (source_filepath_folder_vec)
+      copy_folders(source_filepath = source_filepath_folder_vec, destination_folder = input$destination_folder)
+    }
+    
+    print ("finished copying folders")
+    
+    
+    showModal(modalDialog(HTML(paste0("The files fitting your criteria of depth or string matching are copied to ( ",
+                                      input$destination_folder, " ).  ", "<br>", "<br>", length(source_filepath_vec), " files and ",
+                                      length(source_filepath_folder_vec),  " folders are copied."))))
+    
+    
+    
     print ("Done copying files")
     
-    
+    view(DF_final)
   })
   })
   
